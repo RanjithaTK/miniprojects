b9e8d39b0c764f92f31f51fe5eb42fad4ff59311
im ranjitha <ranjithatk101@gmail.com>
2022-03-22 23:02:43 +0530

    changes2

--- react_native_basics/DoneWithIt/App.js
+++ react_native_basics/DoneWithIt/App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 
 export default function App() {
+  console.log("App executed");
+  const handlePress = () => console.log("pressesd");
+  console.log(require("./assets/favicon.png"));
   return (
     <View style={styles.container}>
-      <Text>this is ranjitha</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        this is ranjitha
+      </Text>
+      <TouchableHighlight>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
